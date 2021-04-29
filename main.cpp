a43838ed4012aa607ae558d733a4800346ca89cf
VedantWairale <74867303+VedantWairale@users.noreply.github.com>
2021-04-30 00:42:40 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+ int main() { 
+
+     int a , b;
+
+     cout<<"first number"<<endl;
+     cin>> a; 
+
+     cout<<"second number"<<endl;
+     cin>> b;
+
+     cout<<"sum of two number = "<<a + b<<endl;
+
+     cout<<"subtraction of two number = "<<a - b<<endl;
+
+     cout<<"product of two number = "<<a * b<<endl;
+
+     cout<<"divtion of two number = "<<a / b<<endl;
+
+     
+
+
+
+ }
